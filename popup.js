352d2cdfda79f83991b76d876c4e18bbd514413c
jmyh <dmitriy.kozlov.97@mail.ru>
2023-10-14 00:16:11 +0300

    Save switch toggle state

--- popup.js
+++ popup.js
@@ -1,21 +1,14 @@
-document.querySelector(".toggle-checkbox").addEventListener('change', function() {
-    (this.checked) ? showData() : hideData();
-});
-
 function showData() {
-    const locatorsStr = localStorage.getItem("locators")
-    if (locatorsStr != null) {
-        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-            chrome.scripting.insertCSS({
-                target: {tabId: tabs[0].id},
-                files: ["onpage/main.css"]
-            });
-            chrome.scripting.executeScript({
-                target: {tabId: tabs[0].id},
-                files: ['onpage/split-page.js', "onpage/show-locators.js", "onpage/show-info.js"]
-            });
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        chrome.scripting.insertCSS({
+            target: {tabId: tabs[0].id},
+            files: ["onpage/main.css"]
         });
-    }
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            files: ['onpage/split-page.js', "onpage/show-locators.js", "onpage/show-info.js"]
+        });
+    });
 }
 
 function hideData() {
@@ -26,3 +19,23 @@ function hideData() {
         });
     });
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    chrome.storage.local.get('toggle', function(data) {
+        if (data.toggle === true)
+            document.querySelector(".toggle-checkbox").checked = data.toggle;
+    });
+
+    chrome.storage.onChanged.addListener(function(changes, areaName){
+        if(areaName === "local" && changes.toggle) {
+            if(changes.toggle.newValue)
+                showData()
+            else
+                hideData()
+        }
+    });
+});
+
+document.querySelector(".toggle-checkbox").addEventListener('change', function() {
+    chrome.storage.local.set({"toggle": this.checked})
+});
